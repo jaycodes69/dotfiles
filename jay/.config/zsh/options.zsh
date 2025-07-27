02ff4bebcc236bda3a4661d8adc4fc97f17e252c
Jaykrishna T J <jaycodes69@protonmail.com>
2025-07-27 11:20:04 +0530

    zsh

--- jay/.config/zsh/options.zsh
+++ jay/.config/zsh/options.zsh
@@ -6,7 +6,6 @@ setopt share_history hist_ignore_dups inc_append_history
 
 # Zsh options
 setopt autocd extendedglob nomatch
-bindkey -v  # vi-mode
 
 # Editor
 export EDITOR=nvim
--- jay/.config/zsh/plugins.zsh
+++ jay/.config/zsh/plugins.zsh
@@ -1,13 +1,29 @@
+# Fix for infinite recursion in starship vi-mode support
+autoload -Uz add-zle-hook-widget
+
+# Prevent Starship from double-wrapping `zle-keymap-select`
+if [[ "${widgets[zle-keymap-select]#user:}" == "starship_zle-keymap-select" || \
+      "${widgets[zle-keymap-select]#user:}" == "starship_zle-keymap-select-wrapped" ]]; then
+    zle -N zle-keymap-select ""
+fi
+
 # Zinit installation (run this once if not already installed)
 [[ ! -f ~/.zinit/bin/zinit.zsh ]] && mkdir -p ~/.zinit && git clone https://github.com/zdharma-continuum/zinit ~/.zinit/bin
 
 source ~/.zinit/bin/zinit.zsh
 
 # Plugins
+zinit ice depth=1
+zinit light jeffreytse/zsh-vi-mode
 zinit light zsh-users/zsh-autosuggestions
 zinit light zsh-users/zsh-syntax-highlighting
 
+# Enable vi mode
+bindkey -v
+
+# Prompt
 eval "$(starship init zsh)"
 
+# Zoxide (with j as alias)
 eval "$(zoxide init zsh --cmd j)"
 export _ZO_ECHO=1
